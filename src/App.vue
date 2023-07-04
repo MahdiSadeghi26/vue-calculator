2f4dba0168ab2ba7c15f96d58d5a539715850a8b
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-07-04 18:17:02 +0330

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import HelloWorld from './components/CalcContainer.vue'
+</script>
+
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .container{
+    width: 100vw;
+    height: 100vh;
+    background-color: var(--light-gray);
+  }
+</style>
