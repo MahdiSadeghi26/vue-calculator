ee6638158a8c4308dc616a41ce28dcdd0f97678b
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-07-05 11:27:25 +0330

    root styling and Calccontainer added

---
+++ src/components/CalcContainer.vue
@@ -0,0 +1,23 @@
+<script setup>
+    
+</script>
+<template >
+  <div class="calcContainer">
+    <div class="monitor"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .calcContainer{
+    width: 400px;
+    height: 500px;
+    background-color: var(--dark-gray);
+    padding: 20px;
+    border-radius: 10px;
+    .monitor{
+      width: 100%;
+      height: 20%;
+      background-color: var(--light-gray);
+      border-radius: 10px;
+    }
+  }
+</style>
